c4337fd0564655e97313d776b8c8c27794a1704d
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-12-16 16:47:16 +0100

    TEST - passed some data

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $data = [
+        "name" => "nome a caso"
+    ];
+    return view('home', $data);
 });
